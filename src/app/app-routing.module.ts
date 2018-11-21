9d4c60765146167b4f002f9a3a8ec317f49235c4
Vaja <hendrik.sgries@mailbox.org>
2018-11-21 20:24:37 +0100

    Resolve merge conflict

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,9 @@ const routes: Routes = [
   // { path: '', redirectTo: '/landing', pathMatch: 'full' },
   { path: 'landing', loadChildren: './landing/landing.module#LandingPageModule' },
   { path: 'videochat', loadChildren: './videochat/videochat.module#VideochatPageModule' },
-<<<<<<< HEAD
   { path: 'checkresults', loadChildren: './checkresults/checkresults.module#CheckresultsPageModule' },
-=======
   { path: 'taxdeclaration', loadChildren: './taxdeclaration/taxdeclaration.module#TaxDeclarationModule' },
   { path: 'insurance', loadChildren: './insurance/insurance.module#InsuranceModule' }
->>>>>>> 21aad2638a0517b52ae2cc2190902bfef1962547
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
