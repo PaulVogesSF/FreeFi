e53b65dcc56c38bd0fdefb7ae3edb513d53bb434
Vaja <hendrik.sgries@mailbox.org>
2018-11-21 16:02:49 +0100

    Add tax declaration page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/landing', pathMatch: 'full' },
   { path: 'landing', loadChildren: './landing/landing.module#LandingPageModule' },
   { path: 'videochat', loadChildren: './videochat/videochat.module#VideochatPageModule' },
+  { path: 'taxdeclaration', loadChildren: './taxdeclaration/taxdeclaration.module#TaxDeclarationModule' }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
