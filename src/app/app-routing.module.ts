4bc4dccd0f73a6eac7fad9efc65569682667b15e
Vaja <hendrik.sgries@mailbox.org>
2018-11-21 22:00:51 +0100

    Ensure onboarding screen is loaded only the first time

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
-import { LandingPage } from './landing/landing.page';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RoutingResolverService } from './services/routingresolver.service';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  {
+    path: '',
+    loadChildren: './tabs/tabs.module#TabsPageModule',
+    resolve: [RoutingResolverService],
+  },
   // { path: '', redirectTo: '/landing', pathMatch: 'full' },
   { path: 'landing', loadChildren: './landing/landing.module#LandingPageModule' },
   { path: 'videochat', loadChildren: './videochat/videochat.module#VideochatPageModule' },
   { path: 'checkresults', loadChildren: './checkresults/checkresults.module#CheckresultsPageModule' },
   { path: 'taxdeclaration', loadChildren: './taxdeclaration/taxdeclaration.module#TaxDeclarationModule' },
   { path: 'taxdeclaration1', loadChildren: './taxdeclaration1/taxdeclaration1.module#TaxDeclaration1Module' },
-  { path: 'insurance', loadChildren: './insurance/insurance.module#InsuranceModule' }
+  { path: 'insurance', loadChildren: './insurance/insurance.module#InsuranceModule' },
+  {
+    path: '**',
+    redirectTo: '',
+  },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
